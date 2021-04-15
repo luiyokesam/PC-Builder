dfc1a378fda2c4d60781c37018022fef26c45417
luiyokesam <luiyokesam@live.com>
2021-04-16 00:41:56 +0800

    add vector havent done logout

--- app/src/main/java/com/example/pcbuilder/StockInDetailsFragment.kt
+++ app/src/main/java/com/example/pcbuilder/StockInDetailsFragment.kt
@@ -5,25 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pcbuilder.databinding.FragmentLoginBinding
+import com.example.pcbuilder.databinding.FragmentStockInDetailsBinding
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_stock_in_details.*
 
 class StockInDetailsFragment : Fragment() {
     private var count:Int = 0
+    private lateinit var auth : FirebaseAuth
+    private var binding: FragmentStockInDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        btn_stockin_addqty.setOnClickListener { addQuantity() }
-        btn_stockin_minusqty.setOnClickListener { minusQuantity() }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock_in_details, container, false)
+        //save inflated view to fragmentBinding
+        val fragmentBinding = FragmentStockInDetailsBinding.inflate(inflater, container, false)
+        auth = FirebaseAuth.getInstance()
+
+        fragmentBinding.btnStockinAddqty.setOnClickListener{ addQuantity() }
+        fragmentBinding.btnStockinMinusqty.setOnClickListener{ minusQuantity() }
+
+        //initialize to binding
+        binding = fragmentBinding
+        //create view with inflated fragmentBinding
+        return fragmentBinding.root
     }
 
     private fun addQuantity(){
