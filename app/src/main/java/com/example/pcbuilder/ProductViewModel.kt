a8ece9de824e7d49f16d697f5606786b410fc4e2
Boonping628 <boonpingsor@gmail.com>
2021-04-16 18:08:26 +0800

    ADD product

---
+++ app/src/main/java/com/example/pcbuilder/ProductViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.pcbuilder
+
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pcbuilder.data.NODE_PRODUCTS
+import com.example.pcbuilder.data.Product
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+
+class ProductViewModel: ViewModel() {
+
+    private val dbproducts = FirebaseDatabase.getInstance().getReference(NODE_PRODUCTS)
+
+    private val _result = MutableLiveData<Exception?>()
+    val result: LiveData<Exception?> get() = _result
+
+    private val _product = MutableLiveData<Product>()
+    val product: LiveData<Product> get() = _product
+
+    fun addProduct(product: Product){
+        product.id = dbproducts.push().key
+
+        dbproducts.child(product.id!!).setValue(product).addOnCompleteListener{
+            if(it.isSuccessful){
+                _result.value = null
+            }else{
+                _result.value = it.exception
+            }
+
+        }
+    }
+
+    private val childEventListener = object: ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val product = snapshot.getValue(Product::class.java)
+            product?.id = snapshot.key
+            _product.value = product!!
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+            TODO("Not yet implemented")
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
+    }
+
+    fun getRealtimeUpdate(){
+        dbproducts.addChildEventListener(childEventListener)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        dbproducts.removeEventListener(childEventListener)
+    }
+
+}
---
+++ app/src/main/java/com/example/pcbuilder/data/constants.kt
@@ -0,0 +1,3 @@
+package com.example.pcbuilder.data
+
+const val NODE_PRODUCTS = "products"
