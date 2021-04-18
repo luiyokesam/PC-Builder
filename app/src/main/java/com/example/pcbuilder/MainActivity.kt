e8e046af04ddc6f8c2e3021e1c76336d69d5bb98
luiyokesam <luiyokesam@live.com>
2021-04-18 14:53:31 +0800

    start

--- app/src/main/java/com/example/pcbuilder/MainActivity.kt
+++ app/src/main/java/com/example/pcbuilder/MainActivity.kt
@@ -1,37 +1,26 @@
 package com.example.pcbuilder
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.widget.Toast
 import androidx.activity.viewModels
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.NavController
 import com.example.pcbuilder.login.LoginViewModel
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-    private lateinit var navController: NavController
-    private val viewModel by viewModels<LoginViewModel>()
-    private lateinit var auth : FirebaseAuth
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        auth = FirebaseAuth.getInstance()
-
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -45,26 +34,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        //up button (uncomment after setting up navigation
-//        val navHostFragment = supportFragmentManager
-//                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//
-//        navController = navHostFragment.navController
-//
-//        setupActionBarWithNavController(navController)
-    }
-
-    override fun onBackPressed() {
-        val count = supportFragmentManager.backStackEntryCount;
-
-        if(count == 1) {
-            super.onBackPressed()
-            //moveTaskToBack(true)
-        }
-        else {
-            supportFragmentManager.popBackStack();
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
