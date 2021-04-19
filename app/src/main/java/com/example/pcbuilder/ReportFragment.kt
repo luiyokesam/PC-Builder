2a7cdd6e4cd74fe2d7b3ae5adfb1052bf1407bed
Boonping628 <boonpingsor@gmail.com>
2021-04-19 15:25:09 +0800

    sdd

--- app/src/main/java/com/example/pcbuilder/ReportFragment.kt
+++ app/src/main/java/com/example/pcbuilder/ReportFragment.kt
@@ -5,10 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.pcbuilder.data.Warehouse
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_report.*
+import kotlinx.android.synthetic.main.fragment_report.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 
 class ReportFragment : Fragment() {
 
+    private val reportCollectionRef = Firebase.firestore.collection("warehouse")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +33,38 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        val view =  inflater.inflate(R.layout.fragment_report, container, false)
+
+        view.btn_report_generate.setOnClickListener {
+            retrieveReport()
+        }
+
+        return view
+    }
+
+    private fun retrieveReport() = CoroutineScope(Dispatchers.IO).launch {
+        val rackID = txt_report_search.text.toString()
+        try {
+            val querySnapshot = reportCollectionRef
+                .whereEqualTo("rackid", rackID)
+                .get()
+                .await()
+            val sb = StringBuilder()
+            sb.append("report\n")
+            for(document in querySnapshot.documents) {
+                val warehouse = document.toObject<Warehouse>()
+                if (warehouse != null) {
+                    sb.append("${warehouse.productCode} \t ${warehouse.inQuantity} \t ${warehouse.inDate}\n")
+                }
+            }
+            withContext(Dispatchers.Main) {
+                txt_report_data.text = sb.toString()
+            }
+        } catch(e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(activity, "Successfully Generate Report", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 
