70d042edae23ff46a51da866755b54f90912be0c
Boonping628 <boonpingsor@gmail.com>
2021-04-19 23:26:06 +0800

    sdssdsd

--- app/src/main/java/com/example/pcbuilder/ReportFragment.kt
+++ app/src/main/java/com/example/pcbuilder/ReportFragment.kt
@@ -1,15 +1,21 @@
 package com.example.pcbuilder
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import com.example.pcbuilder.data.Warehouse
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_item_list_add.*
 import kotlinx.android.synthetic.main.fragment_report.*
 import kotlinx.android.synthetic.main.fragment_report.view.*
 import kotlinx.coroutines.CoroutineScope
@@ -80,6 +86,52 @@ class ReportFragment : Fragment() {
             withContext(Dispatchers.Main) {
                 Toast.makeText(activity, "Successfully Generate Report", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    lateinit var btnReportBarcode: ImageButton
+    lateinit var txtReportRackID: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnReportBarcode = view.findViewById(R.id.btn_Scan_Report)
+        txtReportRackID = view.findViewById(R.id.txt_report_search)
+
+
+        btnReportBarcode.setOnClickListener {
+            val intentIntegrator = IntentIntegrator.forSupportFragment(this)
+            intentIntegrator.setBeepEnabled(false)
+            intentIntegrator.setCameraId(0)
+            intentIntegrator.setPrompt("SCAN")
+            intentIntegrator.setBarcodeImageEnabled(false)
+            intentIntegrator.initiateScan()
+
+
+
+        }
+    }
+
+    override fun onActivityResult(
+            requestCode: Int,
+            resultCode: Int,
+            data: Intent?
+    ) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(context, "cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("Fragment", "Scanned from Fragment")
+                Toast.makeText(context, "Scanned -> " + result.contents, Toast.LENGTH_SHORT)
+                        .show()
+
+                txtReportRackID.text = result.contents
+
+                Log.d("Fragment", "$result")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
