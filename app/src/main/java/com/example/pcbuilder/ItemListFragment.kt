416463d083afa67f8358a4de82fdcfea2c6ffb96
Boonping628 <boonpingsor@gmail.com>
2021-04-20 10:39:35 +0800

    sdsa

--- app/src/main/java/com/example/pcbuilder/ItemListFragment.kt
+++ app/src/main/java/com/example/pcbuilder/ItemListFragment.kt
@@ -1,10 +1,15 @@
 package com.example.pcbuilder
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,7 +19,10 @@ import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
+import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_item_list.*
 import kotlinx.android.synthetic.main.fragment_item_list.view.*
+import kotlinx.android.synthetic.main.fragment_stock_in.*
 import kotlinx.coroutines.tasks.await
 
 class ItemListFragment : Fragment() {
@@ -54,4 +62,66 @@ class ItemListFragment : Fragment() {
         super.onDestroy()
         itemListAdapter!!.stopListening()
     }
+
+    lateinit var btnItemBarcode: ImageButton
+    lateinit var txtItemCode: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnItemBarcode = view.findViewById(R.id.btn_itemlist_scan)
+        txtItemCode = view.findViewById(R.id.txt_itemlist_search)
+
+
+        btnItemBarcode.setOnClickListener {
+            val intentIntegrator = IntentIntegrator.forSupportFragment(this)
+            intentIntegrator.setBeepEnabled(false)
+            intentIntegrator.setCameraId(0)
+            intentIntegrator.setPrompt("SCAN")
+            intentIntegrator.setBarcodeImageEnabled(false)
+            intentIntegrator.initiateScan()
+
+        }
+
+    }
+
+    override fun onActivityResult(
+            requestCode: Int,
+            resultCode: Int,
+            data: Intent?
+    ) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(context, "cancelled", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d("Fragment", "Scanned from Fragment")
+                Toast.makeText(context, "Scanned -> " + result.contents, Toast.LENGTH_SHORT)
+                        .show()
+
+                txtItemCode.text = result.contents
+                setUpRecyclerView1(recycler_view_products)
+
+                Log.d("Fragment", "$result")
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    private fun setUpRecyclerView1(recyclerview: RecyclerView) {
+        //val query : Query = collectionReference.whereEqualTo("productCode", "121")
+        val code = txt_itemlist_search.text.toString()
+        val query : Query = collectionReference.whereEqualTo("productCode", code)
+        val firestoreRecyclerOptions: FirestoreRecyclerOptions<ItemLIstModel> = FirestoreRecyclerOptions.Builder<ItemLIstModel>()
+                .setQuery(query, ItemLIstModel::class.java)
+                .build();
+
+        itemListAdapter = ItemListAdapter(firestoreRecyclerOptions);
+
+        recyclerview.layoutManager = LinearLayoutManager(this.context)
+        recyclerview.adapter = itemListAdapter
+    }
+
+
 }
